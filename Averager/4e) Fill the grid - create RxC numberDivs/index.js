25b63a84d6366cc9472228c30ab2508b6d8d5429
Max Erixon <maxerixon112@gmail.com>
2022-02-06 17:28:54 +0100

    Averager, part 4)e of 8 complete.

--- Averager/4e) Fill the grid - create RxC numberDivs/index.js
+++ Averager/4e) Fill the grid - create RxC numberDivs/index.js
@@ -16,3 +16,52 @@ VIDEO:  Record a video where you explain the two different ways (see above) of c
 */
 
 
+
+// TEST
+gridMaker(
+  document.querySelector("#grid"),
+  4,
+  6
+);
+
+
+function createNumberDiv () {
+
+  // Create each numberDiv
+  let numberDiv = document.createElement("div");
+  numberDiv.innerHTML = randomNumber(100);
+
+  return numberDiv;
+
+}
+function gridMaker (gridContainer, R, C) {
+
+  // Update the CSS properties to give the grid the correct dimensions
+  gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+
+
+  // Fill the container with divs
+
+  // WITH NESTED LOOPS
+  // A loop for the C columns
+  // for (let c = 0; c < C; c++) {
+
+  //   // For each column we need to create R rows
+  //   for (let r = 0; r < R; r++) {
+  //     gridContainer.appendChild( createNumberDiv() );
+  //   }
+
+  // }
+
+
+  // WITH ONE LOOP
+  let nTotal = R * C;
+  for (let i = 0; i < nTotal; i++) {
+    gridContainer.appendChild( createNumberDiv() );
+  }
+
+}
+function randomNumber (max) {
+  return Math.floor(max * Math.random());
+}
