8ca6dd5af133e1ce071453590299335c88b642fd
Max Erixon <maxerixon112@gmail.com>
2022-02-06 12:07:07 +0100

    Averager, part 4)c of 8 complete.

--- Averager/4c) Function that creates a grid from values in input fields/index.js
+++ Averager/4c) Function that creates a grid from values in input fields/index.js
@@ -16,3 +16,21 @@ the values in #inputRows and #inputColumns as arguments.
 
 */
 
+function gridMaker(gridContainer, R, C) {
+    gridContainer.style.display = "grid";
+    gridContainer.style.height = "50vh";
+    gridContainer.style.width = "50vh";
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+}
+
+
+document.querySelector("button").addEventListener("click", function () {
+
+    let elementReferens = document.querySelector("#grid");
+    let nRows = document.querySelector("#inputRows").value;
+    let nCols = document.querySelector("#inputCols").value;
+  
+    gridMaker( elementReferens, nRows, nCols );
+  
+  });
