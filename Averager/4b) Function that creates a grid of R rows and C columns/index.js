9a6e138f9ea433149e5bcd7f3f4bd4f09b972bfc
Max Erixon <maxerixon112@gmail.com>
2022-02-06 11:51:32 +0100

    Averager, part 4)b of 8 complete.

--- Averager/4b) Function that creates a grid of R rows and C columns/index.js
+++ Averager/4b) Function that creates a grid of R rows and C columns/index.js
@@ -27,3 +27,19 @@ The HTML-element #grid will however be empty since we have not appended any othe
 
 */
 
+
+
+function gridMaker(gridContainer, R, C) {
+    gridContainer.style.display = "grid";
+    gridContainer.style.height = "50vh";
+    gridContainer.style.width = "50vh";
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+}
+
+gridMaker( document.querySelector("#grid"), 4, 1 );
+
+
+
+
+
